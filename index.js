8a572a47fda746910a28423120b32bf89791ee97
Bruce Ikenna Efika <ikefika@yahoo.com>
2024-07-23 12:39:24 +0100

    Epic 1 - User Story 2 - completed

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-// Import Express Module
-const express = require('express');
+// // Import Express Module
+// const express = require('express');
 
-// //Using import instead of require
-// import { express } from "express"
+//Using import instead of require
+import express from "express"
+
+// Import Helmet
+import helmet from "helmet"
 
 const app = express();
 const port = 3000;
+
+
+
+// Create Middleware using Helmet
+app.use(helmet());
 
 // Declare app.get with status and message
 app.get('/', (req, res) => {
