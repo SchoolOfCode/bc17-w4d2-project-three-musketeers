8648997168171a735372494cd9567a67d3cd3bc0
Richard Lamb <lamby42@live.co.uk>
2024-07-24 14:10:29 +0100

    tried more on user story 1 epic 2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import helmet from "helmet"
 
 import activities from "./activities.json" assert { type: "json"};
 
+import { v4 as uuidv4 } from 'uuid';
 const app = express();
 const port = 3000;
 
@@ -21,11 +22,14 @@ app.use(helmet());
 
 // Declare app.get with status and message
 
+
+
 app.get('/', (req, res) => {
     res.status(200).send("Hello World");
 });
 
 //here we need some sort of error handling if the response cant be returned or their get ws crash. try success, catch fail? respond w/ obj
+//get activities from json file. returns entire array of data. if error, handle with 500 status code and error message.
 app.get('/activities', (req, res) => {
     try{
         res.status(200).json(activities.data);    
@@ -34,11 +38,28 @@ app.get('/activities', (req, res) => {
     }
 });
 
-// Declare app.get with status and message
+//app.post to add new activity to array. error handling if needed.
+//try
+//catch
+//request body is a .json object. post handler just adds it to array
+async function addNewActivity (newActivity) {
+    activities.push(newActivity);
+    return newActivity;
+}
 
-app.get('/hello', (req, res) => {
-    res.status(200).send("Hello World test2");
+
+app.post('/activities', async (req, res) => {
+    try{
+        const newActivity = await addNewActivity(req.body);
+        res.status(200).json(addNewActivity);
+        res.json(activities.data);
+    } catch {
+            res.status(500).json({ error: 'Failed to add new activities' });
+    } 
 });
+
+const uniqueID = uuidv4();
+console.log(uuidv4());
 
 // Declare app.listen and console log message
 app.listen(port, () => console.log(`App listening on port ${port}`));
