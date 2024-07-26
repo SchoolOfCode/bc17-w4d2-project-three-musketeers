8d7b34f72006d32635eda73be22c94ce46fcea5f
Richard Lamb <lamby42@live.co.uk>
2024-07-26 10:44:37 +0100

    added delete function by id, tidied up old commented out code, added in error handling check in post activites for numberical value of activity_duration

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ const app = express();
 // Declare port 3000
 const port = 3000;
 
-const activties2 = {
-    "data": [
-    {
-        "id": "1", 
-        "activity_submitted": "4", 
-        "activity_type": "run", 
-        "activity_duration": "30"
-    }, 
-    {
-        "id": "2", 
-        "activity_submitted": "5", 
-        "activity_type": "swim", 
-        "activity_duration": "45" 
-    },
-    {
-        "id": "3", 
-        "activity_submitted": "6", 
-        "activity_type": "bike", 
-        "activity_duration": "60"
-    }
-]
-}
-
 // Create Middleware using Helmet
 app.use(helmet());
 // Parse JSON bodies (as sent by API clients)
@@ -59,7 +36,10 @@ app.get('/activities', (req, res) => {
 //When I make a POST request to “http://localhost:3000/activities” with a request body containing a JSON object (new activity). the req body will have the following properties: “activity_type" and "activity_duration”."
 //Then the API should save the new activity to the activities.json file giving it a unique “id” (UUID) and activity_submitted (time stamp - Date.now()),
 //Then the the request should succeed, responding with the correct status code and the activity object that I posted as the response body (response.data).
-
+//detect if request body is missing a required field
+//required fields are activity_type and activity_duration
+//also need to detect if req body is in json format
+// if missing activity type, respond error and also specify which field is missing. so different error messages for different missing fields
 app.post('/activities', (req, res) => {
     const id = uuidv4();
     const activity_submitted = Date.now();
@@ -68,21 +48,20 @@ app.post('/activities', (req, res) => {
         activity_submitted,
         ...req.body
     };  
-     
-
     if (!newActivity.activity_type){
         res.status(400).json({ error: 'Failed because activity_type is required' });
-
     }; 
     if (!newActivity.activity_duration){
         res.status(400).json({ error: 'Failed because activity_duration is required' });
-
     }; 
-
     if (typeof newActivity.activity_type !== 'string') {
        return res.status(400).json({
-        error: 'Invalid request format. Ensure activity_type is a string .'
-       });
+        error: 'Invalid request format. Ensure activity_type is a string .'});
+    };
+    if (typeof newActivity.activity_duration !== "number")  {
+        return res.status(400).json({
+            error: 'Invalid request format. Ensure activity_duration is a number .'
+        });
     };
 
 //|| isNaN(activity_duration)   and activity_duration is a number
@@ -91,51 +70,28 @@ app.post('/activities', (req, res) => {
     res.status(201).json(newActivity);
 });
 
-console.log(typeof activties2.data.activity_type) 
-//Given I am a developer who has made an invalid POST request by missing a required request body field like, activity_type or activity_duration,
-//When I inspect the API response,
-//Then the request should fail, responding with the correct status code and a clear error message (response.error).
-//detect if request body is missing a required field
-//required fields are activity_type and activity_duration
-//also need to detect if req body is in json format
-// if missing activity type, respond error and also specify which field is missing. so different error messages for different missing fields
+// Task 5 - When I make a DELETE request to “http://localhost:3000/activities/activity_id_here”
+// Then the API should delete the activity that has a matching activity id in the activities.json file,
+// Then the request should succeed, responding with the correct status code and the deleted activity object in the response body (response.data).
+// Given I am a developer who has made an invalid DELETE request by specifying an id for a activity that does not exist,
+// When I inspect the API response,
+// Then the the API call should fail with the correct response code and a clear error message (response.error).
+//activities.json file contains an array called data with key value pairs - need to target id in array
+//need an if handler for if find = undefined, this means the id searched does not exist
+//more real world - specifying error handling for bad request parameter / endpoint
 
-    
+app.delete(`/activities/:id` , (req , res ) => {
+    const id = req.params.id;
+    const indexPosition = activities.data.findIndex((item) => item.id === id);
 
-// app.post('/activities', (req, res) => { 
-//     const newActivity = req.body;
-//     newActivity.id = uuidv4();
-//     newActivity.activity_submitted = Date.now();
-//     activities.push(newActivity);
-//     res.status(200).json({
-//         message: 'New activity added',
-//         data: newActivity
-//     });
-// }
-
-
-
-//app.post to add new activity to array. error handling if needed.
-//try
-//catch
-//request body is a .json object. post handler just adds it to array
-// async function addNewActivity (newActivity) {
-//     activities.push(newActivity);
-//     return newActivity;
-// }
-
-
-// app.post('/activities', async (req, res) => {
-//     try{
-//         const newActivity = await addNewActivity(req.body);
-//         res.status(200).json({
-//             message: 'New activity added',
-//             data: newActivity
-//         });
-//     } catch {
-//             res.status(500).json({ error: 'Failed to add new activities' });
-//     } 
-// });
+    if (indexPosition === -1) {
+        return res.status(404).json({
+            error: (`Cannot find activity with ID ${id}.`)
+        });
+    }
+    const deletedActivity = activities.data.splice(indexPosition, 1,);
+    return res.status(200).json(deletedActivity);
+    });  
 
 // Declare app.listen and console log message
 app.listen(port, () => console.log(`App listening on port ${port}`));
